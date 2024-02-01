19e01963e3898d484bbfaf5988ef967129ffb7d9
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-02-01 17:34:30 +0700

    123

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,16 +1,16 @@
-import { Controller } from "@nestjs/common"
+import { Controller, UnauthorizedException } from "@nestjs/common"
 import { GrpcMethod } from "@nestjs/microservices"
 import { UserMySqlEntity } from "@database"
 import { InjectRepository } from "@nestjs/typeorm"
 import { Repository } from "typeorm"
-import { MailerService, Sha256Service } from "@global"
-import { SignInInput, SignUpInput } from "./shared"
+import { FirebaseService, MailerService, Sha256Service } from "@global"
+import { InitInput, SignInInput, SignUpInput, VerifyGoogleAccessTokenInput } from "./shared"
 import {
     GrpcAlreadyExistsException,
     GrpcUnauthenticatedException,
     GrpcUnavailableException,
 } from "nestjs-grpc-exceptions"
-import { MessageResponse } from "@common"
+import { MessageResponse, UserKind } from "@common"
 
 @Controller()
 export default class AuthService {
@@ -19,37 +19,66 @@ export default class AuthService {
     private readonly userMySqlRepository: Repository<UserMySqlEntity>,
     private readonly sha256Service: Sha256Service,
     private readonly mailerService: MailerService,
+    private readonly firebaseService: FirebaseService
     ) {}
 
   @GrpcMethod("AuthService", "signIn")
-    async signIn(body: SignInInput): Promise<UserMySqlEntity> {
+    async signIn(input: SignInInput): Promise<UserMySqlEntity> {
         const found = await this.userMySqlRepository.findOneBy({
-            email: body.email,
+            email: input.email,
         })
         if (!found) throw new GrpcUnavailableException("User not found.")
-        if (!this.sha256Service.verifyHash(body.password, found.password))
+        if (!this.sha256Service.verifyHash(input.password, found.password))
             throw new GrpcUnauthenticatedException("Invalid credentials.")
         return found
     }
 
   @GrpcMethod("AuthService", "signUp")
-  async signUp(body: SignUpInput): Promise<MessageResponse> {
+  async signUp(input: SignUpInput): Promise<MessageResponse> {
       const found = await this.userMySqlRepository.findOne({
           where: {
-              email: body.email,
+              email: input.email,
           },
       })
       if (found) {
           throw new GrpcAlreadyExistsException(
-              `User with email ${body.email} has existed.`,
+              `User with email ${input.email} has existed.`,
           )
       }
-      body.password = this.sha256Service.createHash(body.password)
-      const created = await this.userMySqlRepository.save(body)
+      input.password = this.sha256Service.createHash(input.password)
+      const created = await this.userMySqlRepository.save(input)
 
-      await this.mailerService.sendMail(created.userId, body.email)
+      await this.mailerService.sendMail(created.userId, input.email)
       return {
           message: `An user with id ${created.userId} has been created`,
       }
+  }
+
+  
+  @GrpcMethod("AuthService", "init")
+  async init(input: InitInput): Promise<UserMySqlEntity> {
+      return await this.userMySqlRepository.findOneBy({
+          userId: input.userId,
+      })
+  }
+
+  @GrpcMethod("AuthService", "verifyGoogleAccessToken")
+  async verifyGoogleAccessToken(input: VerifyGoogleAccessTokenInput): Promise<UserMySqlEntity> {
+      const decoded = await this.firebaseService.verifyGoogleAccessToken(input.token)
+      if (!decoded)
+          throw new UnauthorizedException("Invalid Google access token.")
+      let found = await this.userMySqlRepository.findOneBy({
+          externalId: decoded.uid,
+      })
+      if (!found) {
+          found = await this.userMySqlRepository.save({
+              externalId: decoded.uid,
+              email: decoded.email,
+              avatarUrl: decoded.picture,
+              phoneNumber: decoded.phone_number,
+              kind: UserKind.Google,
+          })
+      }
+      return found
   }
 }
